65f15e5cbd9f8c5c976f11334f27f025c4e8eb2a
Drew Britton <drewbritton@gmail.com>
2016-05-05 14:25:02 -0700

    Finished UI

---
+++ CPTCAppNew/CPTCAppNew.iOS/EmailViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace CPTCAppNew.iOS
+{
+	partial class EmailViewController : UIViewController
+	{
+		public EmailViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
